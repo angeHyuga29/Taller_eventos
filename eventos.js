a356aad1ef961a3365dc9adcaf8b158407e740b9
Angie_Hyuga29 <angelicagimenezflores94@gmail.com>
2024-08-22 00:07:05 -0300

    Terminado

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(event)
+{ const unDiv = document.getElementById('unDiv');
+    unDiv.addEventListener('click', function(event) {
+        alert('Hola! Soy el div');
+    });
+});
